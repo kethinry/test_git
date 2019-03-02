c8fbde34f08f1b4b10b318e9b82296bcce718c37
王可欣 <wangkexin@wangkexins-macbook-pro.local>
2019-03-02 11:31:58 +0800

    Initial Commit

---
+++ test_git/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test_git
+//
+//  Created by 王可欣 on 2019/3/2.
+//  Copyright © 2019年 王可欣. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
